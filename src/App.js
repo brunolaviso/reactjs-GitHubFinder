1e11a6ed2c1af0ca1e07e43428790c402fd0e4ab
Bruno Laviso <bruno.laviso@hotmail.com>
2020-07-17 11:35:53 -0300

    Hello, Git :smile:

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import Navbar from "./Navbar";
+import axios from "axios";
+import Profile from "./Profile";
+import Repo from "./Repo";
+
+class App extends Component {
+  constructor ()
+  {
+    super();
+    this.state = {
+      github:{
+        url: 'https://api.github.com/users',
+        client_id: 'a5982f8411c812e83a66',
+        client_secret: 'a41d7c44b5a646ddbfdc39fe11a746f631c0e012',
+        count: 7,
+        sort: 'created: asc'
+      },
+      user: [],
+      repos: []
+    }
+  }
+
+  getUser = (e) =>
+  {
+    const user = e.target.value;
+    const { url, client_id, client_secret, count, sort } = this.state.github;
+    axios.get(`${url}/${user}?client_id=${client_id}&client_secret=${client_secret}`).then(({data}) => this.setState({ user: data }));
+    axios.get(`${url}/${user}/repos?per_page=${count}&sort=${sort}&client_id${client_id}&client_secret${client_secret}`).then(({data}) => this.setState({ repos: data }));
+  }
+
+  renderProfile = () => {
+    const { user, repos } = this.state;
+
+    return (
+      <div className="row">
+        <div className="col-md-4">
+          <Profile user={user}/>
+        </div>
+        <div className="col-md-8">
+          {repos.map(repo => <Repo key={repo.name} repo={repo}/>)}
+        </div>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar/>
+        
+        <div className="container">
+          <div className="card card-body">
+            <h1>Pesquisar Usuários do GitHub</h1>
+            <p className="lead">Digite um nome para encontrar usuários e repositórios</p>
+            <input onChange={this.getUser} id="search" type="text" className="form-control" required/>
+          </div>
+          {this.state.user.length !== 0 ? this.renderProfile() : null }
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Profile.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Profile = ({ user }) => (
+    <div className="row">
+        <div className="col-md-4">
+            <div className="card" style={{ width: "18rem" }}>
+                <img className="card-img-top" src={user.avatar_url} alt=""/>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">Repositórios: <span className="badge-success">{ user.public_repos }</span></li>
+                    <li className="list-group-item">Seguidores: <span className="badge-primary">{ user.followers }</span></li>
+                    <li className="list-group-item">Seguindo: <span className="badge-info">{ user.following }</span></li>
+                </ul>
+                <div className="card-body">
+                    <a href="user.html_url" className="btn btn-info btn-block" target="_blank">Ver Perfil</a>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Profile;
